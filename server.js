18aebe9b2e0df66e6484addb255a16a605ba621c
Steven Feingertz <stevenfeingertz@stevens-mbp.attlocal.net>
2018-11-06 15:31:48 -0500

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 
-// NOTES FOR GIT AND GITHUB
+// NOTES FOR GIT (GIT runs locally as a piece of software. this DOES NOT push anything to github)
 /*
 	Step 1. Run 'git init' in the terminal from inside the project directory. This creates the hidden .git file
 	Step 2. Run 'ls -a' in the terminal. This lists files in the project directory and I can confirm that the .git file was created
 	Step 3: Run 'git status' to see a list of folders/files that git sees.
 	Step 4: Use 'git add <<file or folder>>' to add.
+	Step 5: Create a .gitignore file in root of project. Add all files or folders we want to ignore and not have tracked by git
+	Step 6: Add .gitignore to list of tracked files by git (see step 4)
+*/
+
+/*
+	NOTES FOR HEROKU
+	1. Add "start": "node server.js" to scripts in package.json file. This servers 2 purposes: 1. It allows heroku to start the app. 2. Instead of us running node <<filename>> to start our app, we can run npm start
 */
 
 
@@ -18,7 +25,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-
+// FOR HEROKU, THEY SET THE PORT DYNAMICALLY. SO WE NEED TO SET A VARIABLE TO SET THE PORT, OR USE 3000 IF IT DOESN'T EXIST
+const port = process.env.PORT || 3000;
 
 // SETUP EXPRESS AS SERVER
 var app = express();
@@ -50,9 +58,9 @@ app.use((request, response, next) => {
 });
 
 // EXPRESS MIDDLEWARE MAINTENANCE (TAKE SITE DOWN)
-app.use( (request, response, next) => {
-	response.render('maintenance.hbs');
-});
+// app.use( (request, response, next) => {
+	// response.render('maintenance.hbs');
+// });
 
 // EXPRESS MIDDLEWARE RENDER STATIC DIRECTORY
 app.use(express.static(__dirname + '/public')); // serves a static directory
@@ -90,6 +98,6 @@ app.get('/bad',(request, response) => {
 });
 
 // START SERVER ON PORT XXXX
-app.listen(3000, () => {
-	console.log('Server is up on port 3000.')
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}.`);
 });
